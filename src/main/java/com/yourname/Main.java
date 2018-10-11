15b05dede793b0c89be4e0d312e6f7ac3d2f483a
Flame221 <43928831+Flame221@users.noreply.github.com>
2018-10-11 15:56:56 +0300

    Update Main.java

--- src/main/java/com/yourname/Main.java
+++ src/main/java/com/yourname/Main.java
@@ -1,9 +1,6 @@
 package com.yourname;
 
 public class Main {
-    public static void Main (String [] args)
-    {
-        System.out.println("Дмитрий Чёрный, ДКРКМ ДНУ");
-    }
+    public static void Main (String [] args) { System.out.println("Дмитрий Чёрный, ДКРКМ ДНУ");}
 
 }
